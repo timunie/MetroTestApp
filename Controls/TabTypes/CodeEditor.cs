362bafa0f630736febf0953806be9a14fa6cf30a
T4lus <jeremy.rigaud@live.fr>
2020-04-27 14:14:07 +0200

    Initial commit

---
+++ Controls/TabTypes/CodeEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroTestApp.Controls.TabTypes
+{
+    class CodeEditor : MahApps.Metro.Controls.MetroTabItem
+    {
+
+       
+
+        public CodeEditor(string file)
+        {
+            this.Header = Path.GetFileName(file);
+            //this.CloseButtonEnabled = true;
+            //this.Style = (Style)FindResource("TabItem.VisualStudio");
+
+         
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MetroTestApp.Controls.TabTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetroTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CodeEditor newEditor = new CodeEditor("test");
+            //newEditor.CloseButtonEnabled = true;
+            TabContent.Items.Add(newEditor);
+            TabContent.SelectedIndex = TabContent.Items.Count - 1;
+        }
+    }
+}
